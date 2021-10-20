e4fc84c4eaf06fc78303fa0fe4690901e5a0e542
Sunshine Calderon <86927739+Suncalde@users.noreply.github.com>
2021-10-19 20:21:45 -0500

    Update main.py

--- 02_tip_calculator/main.py
+++ 02_tip_calculator/main.py
@@ -13,5 +13,5 @@ tip_percent = tip/100
 tip_total = bill * tip_percent
 bill_total = bill + tip_total
 bill_per_person = bill_total / num_people
-final_amount = round(bill_per_person, 2)
+final_amount = "{:.2f}".format(bill_per_person)
 print(f"Each person should pay: ${final_amount}")
