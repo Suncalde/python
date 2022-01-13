5fcae70c02691db4e792592645055831946ae8b3
Sunshine Calderon <86927739+Suncalde@users.noreply.github.com>
2022-01-12 20:44:14 -0600

    Add files via upload

---
+++ 03_treasure_island/main.py
@@ -0,0 +1,49 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+choice1 = input('You\'re at a crossroad, where do you want to go? Type "right" or "left".:').lower()
+if choice1 == "left":
+  print("swim or wait?")
+else:
+  print("Tiger attacks you. GAME OVER!")
+
+choice2 = input('You are at a river, what do you want to do? "swim" or "wait"?:').lower()
+if choice2 == "wait":
+  print("which door?")
+else:
+  print("The currents are too strong. GAME OVER!")
+
+choice3 = input('Three doors appear, which color door do you choose? The "red", "green", or "yellow" door?:').lower()
+if choice3 == "yellow":
+  print("You have found the treasure. YOU WIN!!!")
+elif choice3 == "red":
+  print("You are burned by fire. GAME OVER!")
+elif choice3 == "green":
+  print("The wild tucans have taken you hostage. GAME OVER!")
+else:
+  print("GAME OVER!!")
